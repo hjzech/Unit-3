afa0176f9f6acd4e1723a873bb13731e865ae6b1
hjzech <hansjoerg.pb@googlemail.com>
2016-01-11 16:02:24 +0100

    Lesson 3

--- Actors/GithubCommanderActor.cs
+++ Actors/GithubCommanderActor.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Akka.Actor;
 using Akka.Routing;
+using System.Linq;
 
 namespace GithubActors.Actors
 {
@@ -67,7 +68,8 @@ namespace GithubActors.Actors
         private void BecomeAsking()
         {
             _canAcceptJobSender = Sender;
-            pendingJobReplies = 3; //the number of routees
+            //pendingJobReplies = 3; //the number of routees
+            pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees()).Result.Members.Count();
             Become(Asking);
         }
 
@@ -109,15 +111,9 @@ namespace GithubActors.Actors
 
         protected override void PreStart()
         {
-            // create three GithubCoordinatorActor instances
-            var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name + "1");
-            var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name + "2");
-            var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name + "3");
-
-            // create a broadcast router who will ask all of them if they're available for work
             _coordinator =
-                Context.ActorOf(Props.Empty.WithRouter(new BroadcastGroup(ActorPaths.GithubCoordinatorActor.Path + "1",
-                    ActorPaths.GithubCoordinatorActor.Path + "2", ActorPaths.GithubCoordinatorActor.Path + "3")));
+                Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()).WithRouter(FromConfig.Instance),
+                ActorPaths.GithubCoordinatorActor.Name);
             base.PreStart();
         }
 
